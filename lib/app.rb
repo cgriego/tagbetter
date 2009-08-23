ff86d3c0c295106d2155b3edf8c3597c87745510
Adam Keys <adam@therealadam.com>
2009-08-22 22:37:52 -0500

    Cookie-based sessions and authentication.

--- lib/app.rb
+++ lib/app.rb
@@ -2,16 +2,20 @@ require 'sinatra'
 
 class TagBetter::App < Sinatra::Base
   
+  enable :sessions
+  
   configure do
     TagBetter.init
   end
   
-  get '/:user/bundles', :provides => 'application/json' do
-    TagBetter::Bundle.for(params[:user], params[:password]).to_json
+  post '/login' do
+    session[:username] = params[:username]
+    session[:password] = params[:password]
+    redirect '/tagbetter.html'
   end
   
-  get '/test' do
-    'OK.'
+  get '/:username/bundles', :provides => 'application/json' do
+    TagBetter::Bundle.for(params[:username], session[:password]).to_json
   end
   
 end
