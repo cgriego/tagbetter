dd71047e3d65213548b7288b640615467225f3ab
Adam Keys <adam@therealadam.com>
2009-08-23 14:12:58 -0500

    The ugliest little test suite in Texas.

---
+++ test/test.sh
@@ -0,0 +1,30 @@
+# Totally simple testing, ya'll. The app has to be running on 
+# tagbetter.local:80 and CouchDB is listening on localhost:5984. Further, you 
+# need a configuration file in test/config like so:
+#
+#     username=''
+#     password=''
+#     host=''
+#
+# And you have to run it from the project root (./test/test.sh).
+#
+# Yep. It's janky.
+
+source 'test/config'
+
+curl -i -c tmp/cookies.txt \
+        -d username=$username \
+        -d password=$password \
+        http://$host/login
+
+sleep 1 # For delicious
+
+curl -b tmp/cookies.txt \
+     -H 'Accept: application/json' \
+     http://tagbetter.local/$username/bundles
+
+sleep 1 # For delicious
+
+curl -b tmp/cookies.txt \
+    -H 'Accept: application/json' \
+    http://tagbetter.local/$username/tags
